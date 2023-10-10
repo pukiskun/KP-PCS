d4d489964ec3628faa1e07c83c2d80bf25beccdd
pukiskun <fatalitycloud@gmail.com>
2023-10-10 16:27:18 +0700

    database

--- resources/views/arsip.blade.php
+++ resources/views/arsip.blade.php
@@ -1,3 +1,18 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container">
+        <table class="table table table-striped table-success table-hover">
+            <thead>
+                <tr>
+                    <th>Nopol</th>
+                    <th>Merk</th>
+                    <th>Odo Meter</th>
+                    <th>Fuel</th>
+                    <th>Admin</th>
+                    <th>Tanggal Masuk</th>
+                    <th>action</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 @endsection

04498c9eaedb4b90b46d30c88d881faae46211fb
Wulan <wulansandhi@gmail.com>
2023-10-11 15:50:18 +0700

    datatables done belom server side

--- resources/views/arsip/index.blade.php
+++ resources/views/arsip/index.blade.php
@@ -6,41 +6,46 @@
                 <h4 class="mb-3">{{ $pageTitle }}</h4>
             </div>
             <div class="col-lg-3 col-xl-2">
-                <div class="d-grid gap-2">
+                <div class="d-grid gap-2 mb-5">
                     <a href="{{ route('arsip.create') }}" class="btn btn-secondary">Tambah Data</a>
                 </div>
             </div>
         </div>
-        <table class="table table table-striped table-success table-hover mt-5">
-            <thead>
-                <tr>
-                    <th>Nopol</th>
-                    <th>Merk</th>
-                    <th>Odo Meter</th>
-                    <th>Fuel</th>
-                    <th>Admin</th>
-                    <th>Tanggal Masuk</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($items as $item)
-                    <tr>
-                        <td>{{ $item->nopol }}</td>
-                        <td>{{ $item->merk }}</td>
-                        <td>{{ $item->odo_meter }}</td>
-                        <td>{{ $item->fuel }}</td>
-                        <td>{{ $item->admin }}</td>
-                        <td>{{ $item->created_at }}</td>
-                        <td>@include('arsip.actions')</td>
-                    </tr>
 
-                @empty
+        <div class="table-responsive border p-4 rounded-3 bg-white">
+            <table class="table table-bordered table-hover table-striped mt-5 bg-white" id="arsipTable">
+                <thead>
                     <tr>
-                        <td colspan="7">Tidak ada data</td>
+                        <th>Nopol</th>
+                        <th>Merk</th>
+                        <th>Odo Meter</th>
+                        <th>Fuel</th>
+                        <th>Admin</th>
+                        <th>Tanggal Masuk</th>
+                        <th>Action</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($items as $item)
+                        <tr>
+                            <td>{{ $item->nopol }}</td>
+                            <td>{{ $item->merk }}</td>
+                            <td>{{ $item->odo_meter }}</td>
+                            <td>{{ $item->fuel }}</td>
+                            <td>{{ $item->admin }}</td>
+                            <td>{{ $item->created_at }}</td>
+                            <td>@include('arsip.actions')</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
+@push('scripts')
+    <script type="module">
+        $(document).ready(function() {
+            $('#arsipTable').DataTable();
+        });
+    </script>
+@endpush
